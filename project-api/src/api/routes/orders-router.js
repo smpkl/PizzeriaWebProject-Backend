231f631fb4df48cfdfa185dcfed8471ed637b5a1
Elias Rinne <elias1rinne@gmail.com>
2025-11-17 10:27:35 +0200

    Add order router missing post methods, order controller missing addorder, updateorder and getallusersorders. Fix naming sceme of order model to match other namingsceme for clarity

--- project-api/src/api/routes/orders-router.js
+++ project-api/src/api/routes/orders-router.js
@@ -4,17 +4,20 @@ import express from "express";
 import {
   getAllOrders,
   getOrderById,
-  getOrderByUserId,
+  getAllUsersOrders,
+  addOrder,
+  updateOrder,
 } from "../controllers/orders-controller.js";
 
 const orderRouter = express.Router();
 
 // Routes related to announcements:
-orderRouter.route("/").get(getAllOrders);
+orderRouter.route("/").get(getAllOrders).post(addOrder).put(updateOrder);
 orderRouter.route("/:id").get(getOrderById);
+orderRouter.route("/user/:id").get(getAllUsersOrders); // Get order by user id
 
-orderRouter.route("/user/:id").get(getOrderByUserId); // Get order by user id
 
+//Halutaanko tehdä iha omat status haut, vai hakee kaikki ja mappaa/filteröidä sit oikeesee statuksee kuuluvat?
 orderRouter.route("/status/:status").get(getOrdersByStatus); // Get all orders with a specific status (ostoskori, uusi/saapunut, käsittelyssä, käsitelty, arkistoitu)
 
 export default orderRouter;
