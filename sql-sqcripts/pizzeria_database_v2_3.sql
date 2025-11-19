41d180ac46ccff934ef44ad23b5369ab3001e062
riikka <riikaka@metropolia.fi>
2025-11-19 11:08:20 +0200

    Updated database creation script: No admin table, users-table has role  column and email is unique, deleting category no longer deletes products with the same category and same with users-orders and users-feedbacks.

---
+++ sql-sqcripts/pizzeria_database_v2_3.sql
@@ -0,0 +1,144 @@
+DROP DATABASE IF EXISTS `pizzeria_database`;
+CREATE DATABASE IF NOT EXISTS `pizzeria_database`;
+USE `pizzeria_database`;
+
+-- Dumping structure for taulu pizzeria_database.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL, 
+  `last_name` varchar(50) NOT NULL, 
+  `password` varchar(300) NOT NULL, 
+  `email` varchar(80) NOT NULL UNIQUE,
+  `address` varchar(150) NOT NULL, 
+  `role` varchar(50) NOT NULL, 
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.announcements
+DROP TABLE IF EXISTS `announcements`;
+CREATE TABLE IF NOT EXISTS `announcements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `text` varchar(700) NOT NULL,
+  `image` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.categories
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.coupons
+DROP TABLE IF EXISTS `coupons`;
+CREATE TABLE IF NOT EXISTS `coupons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `coupon` varchar(50) NOT NULL,
+  `discount_percentage` double NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.feedbacks
+DROP TABLE IF EXISTS `feedbacks`;
+CREATE TABLE IF NOT EXISTS `feedbacks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `email` varchar(80) NOT NULL,
+  `feedback` varchar(800) NOT NULL,
+  `status` varchar(80) NOT NULL,
+  `received` datetime NOT NULL,
+  `handled` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__users` (`user_id`),
+  CONSTRAINT `FK__users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.meals
+DROP TABLE IF EXISTS `meals`;
+CREATE TABLE IF NOT EXISTS `meals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.order
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `status` varchar(50) NOT NULL,
+  `order_type` varchar(50) NOT NULL,
+  `delivery_address` varchar(80) NOT NULL,
+  `pizzeria_address` varchar(80) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__user_id` (`user_id`),
+  CONSTRAINT `FK__user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.products
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `ingredients` varchar(700) DEFAULT NULL,
+  `price` double NOT NULL,
+  `category` int(11) DEFAULT NULL,
+  `description` varchar(700) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_products_categories` (`category`),
+  CONSTRAINT `FK_products_categories` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.tags
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `color_hex` varchar(50) DEFAULT NULL,
+  `icon` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.meals_products
+DROP TABLE IF EXISTS `meals_products`;
+CREATE TABLE IF NOT EXISTS `meals_products` (
+  `meal_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`meal_id`,`product_id`),
+  KEY `FK_meal_id` (`meal_id`),
+  KEY `FK_product_id` (`product_id`),
+  CONSTRAINT `FK_meal_id` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.order_products
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE IF NOT EXISTS `order_products` (
+  `product_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`,`order_id`),
+  KEY `FK__order_id` (`order_id`),
+  KEY `FK_product_id` (`product_id`),
+  CONSTRAINT `FK__order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK__product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Dumping structure for taulu pizzeria_database.products_tags
+DROP TABLE IF EXISTS `products_tags`;
+CREATE TABLE IF NOT EXISTS `products_tags` (
+  `product_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`,`tag_id`),
+  KEY `FK_product_id` (`product_id`),
+  KEY `FK_tag_id` (`tag_id`),
+  CONSTRAINT `FK_products_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_tags_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
