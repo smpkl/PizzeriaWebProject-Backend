02830b66f80b8fa2920b50f35ca08c76df675b11
Jere Pyörökivi <jere.pyorokivi@metropolia.fi>
2025-11-25 09:03:59 +0200

    Added missing post, put and delete for announcements

--- project-api/src/api/controllers/announcements-controller.js
+++ project-api/src/api/controllers/announcements-controller.js
@@ -2,6 +2,9 @@ import bcrypt from "bcrypt";
 import {
   findAllAnnouncements,
   findAnnouncementById,
+  addNewAnnouncement,
+  modifyAnnouncementById,
+  removeAnnouncement,
 } from "../models/announcements-model.js";
 
 const getAllAnnouncements = async (req, res) => {
@@ -27,4 +30,62 @@ const getAnnouncementById = async (req, res) => {
   }
 };
 
-export { getAllAnnouncements, getAnnouncementById };
+const postAnnouncement = async (req, res) => {
+  try {
+    const newAnnouncement = req.body;
+    const result = await addNewAnnouncement(newAnnouncement);
+    if (result.announcementId) {
+      res.status(200).json({ message: "New announcement added", result });
+    } else {
+      res.status(400).json({ message: "Could not add announcement" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error adding announcement" });
+  }
+};
+
+const putAnnouncement = async (req, res) => {
+  try {
+    const announcementId = req.params.id;
+    const newAnnouncementInfo = req.body;
+    const result = await modifyAnnouncementById(
+      announcementId,
+      newAnnouncementInfo
+    );
+    if (result.announcementId) {
+      res.status(200).json({ message: "Announcement info updated", result });
+    } else {
+      res.status(400).json({ message: "Could not update announcement" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error updating announcement" });
+  }
+};
+
+const deleteAnnouncement = async (req, res) => {
+  try {
+    const announcementId = req.params.id;
+    const result = await removeAnnouncement(announcementId);
+    console.log(result);
+    if (result.announcementId) {
+      res.status(200).json({
+        message: "Announcement deleted",
+        result,
+      });
+    } else {
+      res.status(400).json({
+        message: "Could not delete announcement",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting announcement" });
+  }
+};
+
+export {
+  getAllAnnouncements,
+  getAnnouncementById,
+  postAnnouncement,
+  putAnnouncement,
+  deleteAnnouncement,
+};
