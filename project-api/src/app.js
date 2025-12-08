b9ee388003d91fc86f3f5aec3b43a5ecdfa01a6f
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2025-12-08 13:18:22 +0200

    add cors to localhost:5173

--- project-api/src/app.js
+++ project-api/src/app.js
@@ -5,7 +5,12 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.static("public"));
