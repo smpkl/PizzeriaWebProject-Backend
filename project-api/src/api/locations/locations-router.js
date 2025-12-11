bf64aaf51a3883b2493fadb18b7eb16913dc099c
Elias Rinne <elias1rinne@gmail.com>
2025-12-11 10:41:35 +0200

    add apidoc for locations

--- project-api/src/api/locations/locations-router.js
+++ project-api/src/api/locations/locations-router.js
@@ -4,8 +4,31 @@ import { getAllLocations, getLocationById } from "./locations-controller.js";
 
 const locationsRouter = express.Router();
 
+/**
+ * @api {get} /locations Get all locations
+ * @apiName GetAllLocations
+ * @apiGroup Locations
+ *
+ * @apiSuccess {String} message Locations found.
+ * @apiSuccess {Object[]} locations List of locations.
+ *
+ * @apiError (500 ServerError) Error getting locations.
+ */
 locationsRouter.route("/").get(getAllLocations);
 
+/**
+ * @api {get} /locations/:id Get location by ID
+ * @apiName GetLocationById
+ * @apiGroup Locations
+ *
+ * @apiParam {Number} id Location ID.
+ *
+ * @apiSuccess {String} message Location found.
+ * @apiSuccess {Object} location Location data.
+ *
+ * @apiError (404 NotFound) Location not found.
+ * @apiError (500 ServerError) Error getting location.
+ */
 locationsRouter.route("/:id").get(getLocationById);
 
 export default locationsRouter;
